535647a605cd5f17bb1e4db67041a2663d360eef
dazacher <dazacher2000@yahoo.com>
2021-07-19 07:33:23 -0700

    Subtype membership and literal membership.

--- src/memberships.adb
+++ src/memberships.adb
@@ -1,6 +1,30 @@
+with Ada.Text_IO;
+with Ada.Numerics.Elementary_Functions;
+
 procedure Memberships is
+   package TIO renames Ada.Text_IO;
+   package Math renames Ada.Numerics.Elementary_Functions;
+   type Suit is (Clubs, Diamonds, Hearts, Spades, No_Trumps);
+   Bid_Suit  : Suit := Clubs;
+   Valid_Bid : Boolean;
+   subtype Trick_Range is Integer range 1 .. 13;
+   Bid_Tricks     : Integer range 0 .. 13 := 0;
+   --  Black_Suit     : Boolean;
+   --  Black_Or_Minor : Boolean;
+   --  NS             : Integer               := 0;
+   --  Is_Even        : Boolean;
+   --  Square_Root    : Integer;
 
 begin
-   --  Insert code here.
-   null;
+   -- Subtype membership
+   Valid_Bid := Bid_Tricks in Trick_Range;
+   Valid_Bid := (for some Bid in Trick_Range => Bid = Bid_Tricks);
+   if not Valid_Bid then
+      TIO.Put_Line ("The hand was passed out.");
+   end if;
+
+   -- single literal membership
+   if No_Trumps not in Bid_Suit then
+      TIO.Put_Line ("No Trumps was not the bid.");
+   end if;
 end Memberships;
